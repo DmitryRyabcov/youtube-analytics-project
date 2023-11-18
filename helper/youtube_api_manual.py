3f95382868af912fabc933592be06e211c3e0232
DmitryRyabcov <ryacov_dima@mail.ru>
2023-11-18 16:09:32 +0300

    Item

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -1,11 +1,8 @@
-import json
 import os
-
-# необходимо установить через: pip install google-api-python-client
+import json
 from googleapiclient.discovery import build
-
-import isodate
-
+import isodate as isodate
+import datetime
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 api_key: str = os.getenv('YT_API_KEY')
@@ -14,7 +11,7 @@ api_key: str = os.getenv('YT_API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 
-def printj(dict_to_print: dict) -> None:
+def printj(dict_to_print: dict):
     """Выводит словарь в json-подобном удобном формате с отступами"""
     print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,18 @@
 
-
 class Channel:
     """Класс для ютуб-канала"""
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel_id: str, channel):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.id = channel_id
+        self.info = channel.info
+        self.title = self.info['items'][0]['snippet']['title']
+        self.description = self.info['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/' + self.info['items'][0]['snippet']['customUrl']
+        self.subscriberCount = int(self.info['items'][0]['statistics']['subscriberCount'])
+        self.videoCount = int(self.info['items'][0]['statistics']['videoCount'])
+        self.viewCount = int(self.info['items'][0]['statistic']['viewCount'])
 
-    def print_info(self) -> None:
+    def print_info(self):
         """Выводит в консоль информацию о канале."""
-        pass
+        return self.info
